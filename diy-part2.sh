ac51082028cc6c62645dbd3b693a1134991faaf5
wilimcn <60496939+wilimcn@users.noreply.github.com>
2022-03-16 10:32:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.200/g' package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt /$(date +%Y.%m.%d) by WINAS /g" package/lean/default-settings/files/zzz-default-settings
